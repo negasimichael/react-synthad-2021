5e0dc2cd055b9723a3af54b15e17a867803e16d1
negasimichael <negasimichael>
2021-03-30 22:43:18 -0500

    commit layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { useAppContext } from '../context'
-
+// style
 const ControlsContainer = styled.div`
   display: flex;
   justify-content: space-between;
